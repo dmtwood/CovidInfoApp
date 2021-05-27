352ca994974dc01b820a0f0bec49a206092bb4b0
dmtwood <dimitri.gevers@gmail.com>
2021-05-27 09:04:06 -0700

    created core Entities -> CovidData, CovidCases, CovidDeaths & CovidVaccinated

--- CovidInfoApp/ViewController.swift
+++ CovidInfoApp/ViewController.swift
@@ -2,8 +2,12 @@
 //  ViewController.swift
 //  CovidInfoApp
 //
-//  Created by student on 27/05/2021.
+//  Copyright © Dimitri Gevers. All rights reserved. Created on 27/05/2021.
 //
+
+/// https://epistat.sciensano.be/Data/COVID19BE_CASES_AGESEX.json -> cases
+/// https://epistat.sciensano.be/Data/COVID19BE_MORT.json -> death count
+/// https://epistat.sciensano.be/Data/COVID19BE_VACC.json -> administered vaccins
 
 import UIKit
 
