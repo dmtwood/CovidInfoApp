de334bf8e1f1380e21bd72ac49c2cb8959b90cef
dmtwood <dimitri.gevers@gmail.com>
2021-06-14 13:24:16 -0700

    func to fetch data from sciensano

--- CovidInfoApp/ViewController.swift
+++ CovidInfoApp/ViewController.swift
@@ -10,14 +10,107 @@
 /// https://epistat.sciensano.be/Data/COVID19BE_VACC.json -> administered vaccins
 
 import UIKit
+import CoreData
+import Charts
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    func getAdministeredVaccins() -> Void {
+        let url = URL(string:  = "https://epistat.sciensano.be/Data/COVID19BE_VACC.json")!
+        
+        let task = URLSession.shared.dataTask(with: url) {
+            (data: Data?, response: URLResponse?, error:Error?) -> Void in
+            if let datafromJson  = data{
+                
+                let jsonDecoder = JSONDecoder();
+                
+                do {
+                    try jsonDecoder.decode(self, from datafromJson)
+                }
+                
+                catch {
+                    print(error)
+                }
+                
+            }
+            self.getData();
+            self.updateCharts();
+        }
+        
+        task.resume()
+    }
+    
+    
+    
+    func getNumberOfDeaths() -> Void {
+        let url = URL(string:  = "https://epistat.sciensano.be/Data/COVID19BE_MORT.json")!
+        
+        let task = URLSession.shared.dataTask(with: url) {
+            (data: Data?, response: URLResponse?, error:Error?) -> Void in
+            if let datafromJson  = data{
+                
+                let jsonDecoder = JSONDecoder();
+                
+                do {
+                    try jsonDecoder.decode(self, from datafromJson)
+                }
+                
+                catch {
+                    print(error)
+                }
+                
+            }
+            self.getData();
+            self.updateCharts();
+        }
+        
+        task.resume()
+    }
+    func getNumberOfInfections() -> Void {
+        let url = URL(string:  "https://epistat.sciensano.be/Data/COVID19BE_CASES_AGESEX.json")!
+        
+        let task = URLSession.shared.dataTask(with: url) {
+            (data: Data?, response: URLResponse?, error:Error?) -> Void in
+            if let datafromJson  = data{
+                
+                let jsonDecoder = JSONDecoder();
+                
+                do {
+                    try jsonDecoder.decode(self, from: datafromJson)
+                }
+                
+                catch {
+                    print(error)
+                }
+                
+            }
+            self.getData();
+            self.updateCharts();
+        }
+        
+        task.resume()
+    }
+    
+    
+    
+    func
+    getData() -> Void {
+    // try.self.context.fetch();
+   }
+    
+    func updateCharts() {
+        
+    }
+   
+   
 }
 
+
+ac
